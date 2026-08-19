2b3fe5ff49e7507e7c5a56b68a5fbe991adf3e6b
desu <git@desu.re>
2026-08-19 19:18:30 +0200

    fix(build): pin zstd to sisong fork per hdiffpatch, add zstd_preSplit.c
    hdiffpatch v5.1.3's Makefile compiles from https://github.com/sisong/zstd
    (a fork of facebook/zstd, deltaUpdateDict branch = v1.5.7 + hsynz
    delta-dict API). The submodule was pointed at facebook/zstd at a recent
    master (v1.4.7-3035) and the file list omitted compress/zstd_preSplit.c,
    which defines ZSTD_splitBlock referenced by zstd_compress.c, causing a
    link failure.
    
    - repoint vendor/zstd to https://github.com/sisong/zstd
    - lock submodule at 68c88c7c (deltaUpdateDict, zstd 1.5.7)
    - add compress/zstd_preSplit.c to ZSTD_C_FILES in build.rs

--- build.rs
+++ build.rs
@@ -173,6 +173,7 @@ const ZSTD_C_FILES: &[&str] = &[
     "compress/zstd_compress_literals.c",
     "compress/zstd_compress_sequences.c",
     "compress/zstd_compress_superblock.c",
+    "compress/zstd_preSplit.c",
     "compress/zstd_double_fast.c",
     "compress/zstd_fast.c",
     "compress/zstd_lazy.c",
